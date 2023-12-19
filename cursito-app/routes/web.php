ae979261cfc419da6d344ef0517bae28d2731d93
Juan Alcoba <jc.alcoba@gmail.com>
2023-12-19 17:44:21 -0300

    Crud Listado y Delete

--- cursito-app/routes/web.php
+++ cursito-app/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
 
 /*
 
@@ -31,5 +32,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('posts', PostController::class)->except(['show']);
 
 require __DIR__.'/auth.php';
