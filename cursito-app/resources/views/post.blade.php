35407166c5c513488a4c18717570c9e3751fc38c
Juan Alcoba <jc.alcoba@gmail.com>
2024-03-01 10:13:44 -0300

    Posts links and styling

--- cursito-app/resources/views/post.blade.php
+++ cursito-app/resources/views/post.blade.php
@@ -1,7 +1,10 @@
 @extends('template')
 
 @section('content')
-    <h1>{{ $post->titulo }}</h1>
-    <p> {{ $post->body }} </p>
+
+    <div class="max-w-3xl mx-auto">
+        <h1 class="text-5xl mb-8">{{ $post->titulo }}</h1>
+        <p class="leading-loose text-lg text-gray-700"> {{ $post->body }} </p>
+    </div>
 @endsection
 
